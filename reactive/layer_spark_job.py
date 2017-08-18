9c53da6e1d0b4b3b30d316b125c840b777fe964d
sebastienpattyn93 <sebastien.pattyn@gmail.com>
2017-08-18 11:33:20 +0200

    changed icon + wrote deployment code

--- reactive/layer_spark_job.py
+++ reactive/layer_spark_job.py
@@ -1,17 +1,34 @@
-from charms.reactive import when, when_not, set_state
+#!/usr/bin/env python3
+# Copyright (C) 2017  Qrama
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# pylint: disable=c0111,c0103,c0301,c0412
+import subprocess as sp
+import tempfile
+
+from charms.reactive import when_not, set_state
+from charmhelpers.core.hookenv import status_set, config, service_name
 
 
-@when_not('layer-spark-job.installed')
-def install_layer_spark_job():
-    # Do your setup here.
-    #
-    # If your charm has other dependencies before it can install,
-    # add those as @when() clauses above., or as additional @when()
-    # decorated handlers below
-    #
-    # See the following for information about reactive charms:
-    #
-    #  * https://jujucharms.com/docs/devel/developer-getting-started
-    #  * https://github.com/juju-solutions/layer-basic#overview
-    #
-    set_state('layer-spark-job.installed')
+@when_not('spark-job.installed')
+def deploy_job():
+    conf = config()
+    if not conf['job_location']:
+        tmp_dir = tempfile.mkdtemp()
+        sp.check_call(['wget', conf['job_location'], '-P', tmp_dir])
+        sp.check_call(['./bin/spark-submit', tmp_dir])
+        status_set('active', 'Spark job {} is deployed and running on spark'.format(service_name()))
+        set_state('spark-job.installed')
+    else:
+        status_set('blocked', 'Please provide a valid url to deploy job by changing job_location config')
